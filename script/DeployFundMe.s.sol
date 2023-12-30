73f7ffc73af17859d112dd16aeaeaba2b5bb8b97
mp-web3 <mp.web3.t@gmail.com>
2023-12-30 19:20:49 +0100

    refactored-3-mock-priceFeed

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -6,12 +6,6 @@ import {Script} from "forge-std/Script.sol";
 import {FundMe} from "contracts/fundMe/FundMe.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
 
-/// Lesson 7 Part 9 | Refactoring II Helper Config - Solidity & Foundry Full Course
-/// https://www.youtube.com/watch?v=YqnxsefqO5A&list=PL2-Nvp2Kn0FPH2xU3IbKrrkae-VVXs1vk&index=89
-// In this second refactoring we want to avoid hard coding priceFeed address, and also avoid having to 
-// go through Alchemy SEPOLIA RPC ENDPOINT everytime we run a test (we don't want to run out of API calls)
-// We are going to deploy a Mock Contract of 
-
 contract DeployFundMe is Script{
     function run() external returns (FundMe) {
 
